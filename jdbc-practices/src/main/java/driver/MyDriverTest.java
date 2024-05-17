fb3e6e2db5f4d79c89fde05556f40bb20663614b
cksghks89 <song960705@naver.com>
2024-05-17 13:30:05 +0900

    database - JDBC MyDriver

--- jdbc-practices/src/main/java/driver/MyDriverTest.java
+++ jdbc-practices/src/main/java/driver/MyDriverTest.java
@@ -1,18 +1,18 @@
-package test;
+package driver;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionTest {
+public class MyDriverTest {
 	public static void main(String[] args) {
 		Connection connection = null;
 		try {
 			// 1. JDBC Driver 로딩
-			Class.forName("org.mariadb.jdbc.Driver");
+			Class.forName("driver.MyDriver");
 			
 			// 2. 연결하기
-			String url = "jdbc:mariadb://192.168.64.3:3306/webdb?charset=utf8";
+			String url = "jdbc:mydb://127.0.0.1:2202/webdb";
 			connection = DriverManager.getConnection(url, "webdb", "webdb");
 			
 			System.out.println("success!!");
