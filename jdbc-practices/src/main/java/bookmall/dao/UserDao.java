e43156613de2aa7549138f0cec7cb088fa721a46
cksghks89 <song960705@naver.com>
2024-05-20 17:21:22 +0900

    MariaDB 데이터베이스 모델링, JDBC Programming 평가과제

---
+++ jdbc-practices/src/main/java/bookmall/dao/UserDao.java
@@ -0,0 +1,97 @@
+package bookmall.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import bookmall.vo.UserVo;
+import bookshop.vo.BookVo;
+
+public class UserDao {
+	private Connection getConnection() throws SQLException {
+		Connection conn = null;
+
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+
+			String url = "jdbc:mariadb://192.168.64.3:3306/bookmall?charset=utf8";
+			conn = DriverManager.getConnection(url, "bookmall", "bookmall");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		}
+
+		return conn;
+	}
+	
+	public int insert(UserVo vo) {
+		int result = 0;
+		
+		try (
+				Connection conn = getConnection();
+				PreparedStatement pstmt1 = conn.prepareStatement("insert into user(name, phone, email, password) values (?, ?, ?, ?)");
+				PreparedStatement pstmt2 = conn.prepareStatement("select last_insert_id() from dual");
+		) {
+			pstmt1.setString(1, vo.getName());
+			pstmt1.setString(2, vo.getPhone());
+			pstmt1.setString(3, vo.getEmail());
+			pstmt1.setString(4, vo.getPassword());
+			
+			result = pstmt1.executeUpdate();
+			
+			ResultSet rs = pstmt2.executeQuery();
+			vo.setNo(rs.next() ? rs.getLong(1) : null);
+		} catch (SQLException e) {
+			System.out.println("error : " + e);
+		}
+
+		return result;
+	}
+	
+	public List<UserVo> findAll() {
+		List<UserVo> result = new ArrayList<>();
+
+		try (
+			Connection conn = getConnection();
+			PreparedStatement pstmt = conn.prepareStatement("select no, name, phone, email, password from user order by no");
+			ResultSet rs = pstmt.executeQuery();
+		) {
+			while (rs.next()) {
+				Long no = rs.getLong(1);
+				String name = rs.getString(2);
+				String phone = rs.getString(3);
+				String email = rs.getString(4);
+				String password = rs.getString(5);
+
+				UserVo vo = new UserVo(name, email, password, phone);
+				vo.setNo(no);
+				
+				result.add(vo);
+			}
+		} catch (SQLException e) {
+			System.out.println("error : " + e);
+		}
+
+		return result;
+	}
+
+	public int deleteByNo(Long no) {
+		int result = 0;
+		
+		try (
+				Connection conn = getConnection();
+				PreparedStatement pstmt = conn.prepareStatement("delete from user where no = ?");
+		) {
+			pstmt.setLong(1, no);
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("error : " + e);
+		}
+		
+		return result;
+	}
+
+}
---
+++ jdbc-practices/src/main/java/bookmall/vo/OrderBookVo.java
@@ -0,0 +1,50 @@
+package bookmall.vo;
+
+public class OrderBookVo {
+	private Long orderNo;
+	private Long bookNo;
+	private String bookTitle;
+	private Integer quantity;
+	private Integer price;
+
+	public Long getOrderNo() {
+		return orderNo;
+	}
+
+	public void setOrderNo(Long orderNo) {
+		this.orderNo = orderNo;
+	}
+
+	public Long getBookNo() {
+		return bookNo;
+	}
+
+	public void setBookNo(Long bookNo) {
+		this.bookNo = bookNo;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public String getBookTitle() {
+		return bookTitle;
+	}
+
+	public void setBookTitle(String bookTitle) {
+		this.bookTitle = bookTitle;
+	}
+
+}
